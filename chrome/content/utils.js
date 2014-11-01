57a01777de82026d680c9c335fc832371c2d2cd8
Infocatcher <infocatcher.dev.null@gmail.com>
2014-11-01 20:41:08 +0300

    Sync losslessDecodeURI() with latest Firefox 36.0a1

--- chrome/content/utils.js
+++ chrome/content/utils.js
@@ -127,7 +127,7 @@ var linkPropsPlusUtils = {
 		}
 
 		// Based on losslessDecodeURI() function from
-		// chrome://browser/content/browser.js in Firefox 19.0a1 (2012-11-05)
+		// chrome://browser/content/browser.js in Firefox 36.0a1 (2014-11-01)
 
 		// Try to decode as UTF-8 if there's no encoding sequence that we would break.
 		if(!/%25(?:3B|2F|3F|3A|40|26|3D|2B|24|2C|23)/i.test(uri)) try {
@@ -141,20 +141,21 @@ var linkPropsPlusUtils = {
 				//    by the location bar (bug 410726).
 				.replace(/%(?!3B|2F|3F|3A|40|26|3D|2B|24|2C|23)|[\r\n\t]/ig, encodeURIComponent);
 		}
-		catch (e) {
+		catch(e) {
 			Components.utils.reportError(e);
 		}
 
-		// Encode invisible characters (line and paragraph separator,
-		// object replacement character) (bug 452979)
-		uri = uri.replace(/[\v\x0c\x1c\x1d\x1e\x1f\u2028\u2029\ufffc]/g, encodeURIComponent);
+		// Encode invisible characters (C0/C1 control characters, U+007F [DEL],
+		// U+00A0 [no-break space], line and paragraph separator,
+		// object replacement character) (bug 452979, bug 909264)
+		uri = uri.replace(/[\u0000-\u001f\u007f-\u00a0\u2028\u2029\ufffc]/g, encodeURIComponent);
 
 		// Encode default ignorable characters (bug 546013)
 		// except ZWNJ (U+200C) and ZWJ (U+200D) (bug 582186).
 		// This includes all bidirectional formatting characters.
 		// (RFC 3987 sections 3.2 and 4.1 paragraph 6)
 		uri = uri.replace(
-			/[\u00ad\u034f\u115f-\u1160\u17b4-\u17b5\u180b-\u180d\u200b\u200e-\u200f\u202a-\u202e\u2060-\u206f\u3164\ufe00-\ufe0f\ufeff\uffa0\ufff0-\ufff8]|\ud834[\udd73-\udd7a]|[\udb40-\udb43][\udc00-\udfff]/g,
+			/[\u00ad\u034f\u061c\u115f-\u1160\u17b4-\u17b5\u180b-\u180d\u200b\u200e-\u200f\u202a-\u202e\u2060-\u206f\u3164\ufe00-\ufe0f\ufeff\uffa0\ufff0-\ufff8]|\ud834[\udd73-\udd7a]|[\udb40-\udb43][\udc00-\udfff]/g,
 			encodeURIComponent
 		);
 		return uri;
